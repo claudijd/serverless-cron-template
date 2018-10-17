60652f23ab57bf0dd0bacef94982adc0b10b776c
Jonathan Claudius <jclaudius@mozilla.com>
2018-10-17 08:17:47 -0700

    Fix up S3 put

--- http-observatory/handler.py
+++ http-observatory/handler.py
@@ -13,8 +13,8 @@ logger.setLevel(logging.INFO)
 def run(event, context):
     # Test out S3 upload capability
     url = 'https://raw.githubusercontent.com/mozilla/http-observatory-dashboard/master/httpobsdashboard/conf/sites.json'
-    random_site = Randomizer(url)
+    hostname = Randomizer(url)
     scanner = ObservatoryScanner()
-    scan_result = scanner.scan(random_site.next())
+    scan_result = scanner.scan(hostname.next())
     logger.info(scan_result)
     send_to_s3(hostname, scan_result)
